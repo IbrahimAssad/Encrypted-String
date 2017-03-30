947aa8dd111736c8e231bc78407524a586f8c73a
Ibrahim Assad <Ibrahim.assad@tajawal.com>
2017-03-30 19:38:40 +0000

    add Constant

---
+++ src/Constants.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Iassad\Encrypt;
+
+/**
+ * Class Constants
+ *
+ * @package Iassad\Encrypt
+ */
+class Constants
+{
+    /** int ascii for A */
+    public const  A_LETTER = 65;
+    /** int ascii for Z */
+    public const  Z_LETTER = 90;
+    /** int ascii for a */
+    public const  a_LETTER = 97;
+    /** int ascii for z */
+    public const  z_LETTER = 122;
+    /** int ascii for 0 */
+    public const  ZERO_NUMBER = 48;
+    /** int ascii for 9 */
+    public const  NINE_NUMBER = 57;
+
+}
